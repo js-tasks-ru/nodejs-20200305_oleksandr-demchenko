3beb199bebd669a49392dd457519fdf79e74c088
ab0nent <7582701@gmail.com>
2020-03-01 14:11:12 +0200

    add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,12 @@
 function sum(a, b) {
-  /* ваш код */
+  if (isNumeric(a) && isNumeric(b)) {
+    return a + b;
+  }
+  throw new TypeError();
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 module.exports = sum;
