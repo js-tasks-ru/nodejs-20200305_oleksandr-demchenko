dcf4cc94ed3085985320e45041a70ab987f63019
ab0nent <7582701@gmail.com>
2020-03-20 19:11:03 +0200

    add solution for 4-module 1-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -11,6 +13,42 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('File does not exist');
+        return;
+      }
+
+      if (fs.existsSync(filepath)) {
+        res.statusCode = 409;
+        res.end('File already exists');
+        return;
+      }
+
+      const writeStream = fs.createWriteStream(filepath);
+      const limitStream = new LimitSizeStream({limit: 1024 * 1024});
+
+      req.pipe(limitStream).pipe(writeStream);
+
+      limitStream.on('error', () => {
+        if (fs.existsSync(filepath)) {
+          fs.unlink(filepath, (e) => {
+            res.statusCode = 413;
+            res.end('File limited to 1MB');
+          });
+        }
+      });
+
+      req.on('aborted', () => {
+        writeStream.destroy();
+        fs.unlink(filepath, () => {});
+      });
+
+      req.on('end', () => {
+        res.statusCode = 201;
+        res.end();
+      });
 
       break;
 
